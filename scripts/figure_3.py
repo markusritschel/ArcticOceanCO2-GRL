9941e35f670b2336642da4047667c8fd8d1d8a6f
markusritschel <git@markusritschel.de>
2024-07-31 14:34:56 +0200

    Add script for figure 3

---
+++ scripts/figure_3.py
@@ -0,0 +1,229 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-07-30
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import calendar
+import logging
+import cartopy.crs as ccrs
+import cmocean
+import numpy as np
+import pandas as pd
+import xarray as xr
+import xarrayutils
+import xeofs
+from matplotlib import pyplot as plt
+import mpl_axes_aligner
+
+from src import BASE_DIR, DATA_DIR, PLOT_DIR
+from src.core.utils import save
+from src.helper.datahandling import mask_xarray
+from my_code_base.plot.maps import *
+
+
+log = logging.getLogger(__name__)
+
+plt.style.use(BASE_DIR/"assets/mpl_styles/white_paper.mplstyle")
+
+
+def main(modes=[1,2]):
+    n_modes = len(modes)
+    data = read_data()
+    data = prepare_data(data)
+    eofs, pcs, expvar = perform_eof_analysis(data, n_modes=10)
+    fig = prepare_figure(modes)
+
+    for i, mode in enumerate(modes):
+        fig_eof = fig.subfigs[0]
+        fig_pc = fig.subfigs[1]
+
+        eof_ax = fig_eof.axes[i]
+        pc_ax = fig_pc.axes[n_modes*(i+1)]
+        pc_seas_ax = fig_pc.axes[n_modes*(i+1)+1]
+        eof = eofs.sel(mode=mode)
+        pc = pcs.sel(mode=mode)
+        plot_eof_map(eof=eof, ax=eof_ax)
+        plot_pval_hatch(pc, data, ax=eof_ax)
+        plot_principal_component(pc=pc, ax=pc_ax)
+        plot_pc_seasonality(pc=pc, ax=pc_seas_ax)
+        # plot_homogenous_map(ax=hom_map_ax)
+    save(fig, PLOT_DIR/"figure_3.png", add_hash=True)
+
+
+def prepare_figure(modes):
+    fig = plt.figure(figsize=(12, 8), layout='constrained')
+    fig_eof, fig_pc = fig.subfigures(1, 2, width_ratios=[1, 2])
+
+    fig_eof.subplots(2, 1, subplot_kw=dict(projection=ccrs.NorthPolarStereo()))
+    fig_pc.subplots(4, 2, width_ratios=[5, 3], height_ratios=[1, 2, 2, 1], sharey=True)
+    fig_pc.axes[0].set_axis_off()
+    fig_pc.axes[1].set_axis_off()
+    fig_pc.axes[-2].set_axis_off()
+    fig_pc.axes[-1].set_axis_off()
+    return fig
+
+def prepare_figure2(modes):
+    fig = plt.figure(figsize=(12, 8), layout='constrained')
+    gs = fig.add_gridspec(len(modes), 3, width_ratios=[3,3,2])
+    
+    for i in range(len(modes)):
+        fig.add_subplot(gs[i, 0], projection=ccrs.NorthPolarStereo())
+        fig.add_subplot(gs[i, 1])
+        fig.add_subplot(gs[i, 2])
+        # fig.add_subplot(gs[i, 3], projection=ccrs.NorthPolarStereo())
+
+    return fig
+
+
+def read_data():
+    input_file = (DATA_DIR/f"raw/merged/observations/pco2/monthly/").glob(f"pco2_MPIM-SOM-FFN_*.nc")
+    data = xr.open_dataset(next(input_file))
+    data = data['pco2'].sel(time=slice('2005', '2022')).load()
+    data['time'] = pd.to_datetime(data.time) + pd.DateOffset(days=-14)
+    return data
+
+
+def prepare_data(data: xr.DataArray):
+    """
+    Prepare the data for the EOF analysis:
+
+    - Remove linear trend of each grid point. This also centers the data around 0, i.e. it 
+    removes the long-term mean.
+    - Mask out the Baltic Sea
+    - Restrict the domain to the Arctic
+    - Fill NaNs with the mean of the detrended time series (--> 0) to remove variance
+    """
+    data = xarrayutils.xr_detrend(data, dim='time')
+
+    # mask out the Baltic Sea
+    masks = mask_xarray(data, BASE_DIR/"assets/arctic-regions/arctic-regions.shp")
+    data = data.where(~masks.sel(domain='Baltic Sea'))
+
+    # Restrict the domain to the Arctic
+    data = data.sel(lat=slice(50, 90))
+
+    # Fill NaNs with the mean of the detrended time series (--> 0) to remove variance
+    data = data.fillna(data.mean('time'))
+
+    return data
+
+
+def perform_eof_analysis(data, n_modes=10):
+    model = xeofs.models.EOF(n_modes=n_modes, standardize=False, center=True, use_coslat=True)
+    model.fit(data, dim=['time'])
+
+    rotator = xeofs.models.EOFRotator(n_modes=n_modes, power=1)
+    rotator.fit(model)
+
+    expvar = rotator.explained_variance_ratio() * 100
+
+    try:
+        n_PC = np.where(expvar.cumsum() >= 70)[0][0] + 1
+        print(f"The first {n_PC} modes explain at least 70% of the variance.")
+    except:
+        log.warning(f"Less than 70% of the variance explained by the first {len(expvar)} EOFs.")
+
+    plot_expvar(expvar)
+    
+    eofs = rotator.components()
+    pcs = rotator.scores()
+
+    return eofs, pcs, expvar
+
+
+def plot_expvar(expvar):
+    fig = plt.figure()
+    expvar.plot(marker='.', lw=.5, ax=plt.gca())
+    plt.title('')
+    save(fig, PLOT_DIR/"eof_analysis_explained_variance.png")
+
+
+def plot_eof_map(eof, ax):
+    eof.plot(ax=ax, transform=ccrs.PlateCarree(), cmap=cmocean.cm.balance, add_colorbar=False)
+    ax.set_title('')
+    ax.polar.add_features(labels=False, 
+                          ruler_kwargs={'primary_color': '#808080', 
+                                        'secondary_color': '#EFEFDA'})
+
+
+def plot_pval_hatch(pc, data, ax):
+    pval = xarrayutils.xr_linregress(pc, data, dim='time').p_value
+    pval.where(pval < .05).plot.contourf(ax=ax, transform=ccrs.PlateCarree(),
+                                            add_colorbar=False,
+                                            # levels=[.05,1], lw=2, colors='w'
+                                            # )
+                                            cmap='none',  # alpha=.7,
+                                            hatches=['...'])
+    ax.set_title('')
+
+
+def plot_principal_component(pc, ax):
+    pc.plot(ax=ax, c='k')
+    ax.set_title('')
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    # ax.set_title('')
+
+
+def plot_pc_seasonality(pc, ax):
+    ax.set_xticks(np.arange(12)+1, [calendar.month_abbr[m][0] for m in np.arange(12)+1])
+    ax_sia = plt.twinx(ax)
+    seasonal_pc = calc_seasonality(pc.to_pandas())
+    get_nyr_averages(seasonal_pc.T, 4).T.plot(ax=ax, cmap='PuRd', legend=False)
+
+    add_sia_to_pc_seasonality(ax_sia)
+    mpl_axes_aligner.align.yaxes(ax, 0, ax_sia, 6.5)
+
+    ax.set_title('')
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+
+
+def add_sia_to_pc_seasonality(ax):
+    ice_da = xr.open_dataset("/home/markusritschel/PycharmProjects/corsica/data/processed/monthly/observations_siarea.nc").sel(time=slice("2005", "2021")).load().siarea
+    sia_da = ice_da.sel(region='arctic', member='NASA-Team')
+    sia_da['time'] = pd.to_datetime(sia_da.time) + pd.DateOffset(days=-14)
+
+    sia_seas_offset = sia_da.mean('time').values
+    seasonal_sia = calc_seasonality(sia_da.interpolate_na('time').to_pandas()) + sia_seas_offset
+    
+    seasonal_sia.mean(axis=1).plot(ax=ax, ls='--', lw=1, c='.5', zorder=0, legend=False)
+
+    ax.set_ylabel("SIA [million km²]")
+    ax.annotate("SIA\n[million km²]", xy=(5, seasonal_sia.mean(axis=1)[5]), xytext=(.5,.9),
+                textcoords=ax.transAxes,
+                ha='left', va='top',
+                color='.5',
+                fontsize='x-small',
+                arrowprops=dict(arrowstyle="-", lw=.5, color='.5'),
+                )
+
+
+def get_nyr_averages(df, n=5):
+    """df should have dimensions (rows: years, cols: months)"""
+    grouped = df.groupby((df.index - df.index[0])//n)
+    df_avg = grouped.mean()
+    df_avg.index = [f"{v.index[0]} – {v.index[-1]}" for k, v in grouped]
+    return df_avg
+
+
+def calc_seasonality(df):
+    from statsmodels.tsa.stl._stl import STL
+    results = STL(df, period=12, robust=True).fit()
+
+    seasonal = results.seasonal.to_frame()
+    seasonal['month'] = seasonal.index.month
+    seasonal['year'] = seasonal.index.year
+    seasonal = seasonal.pivot(columns='year', index='month')
+
+    return seasonal.droplevel(0, axis=1)#.mean(axis=1)
+
+
+def plot_homogenous_map(ax):
+    ...
+
+
+
+if __name__ == "__main__":
+    main()

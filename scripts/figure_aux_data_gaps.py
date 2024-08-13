deb651b6b56e6924ec1a48c29960ca0b3c7289bf
markusritschel <git@markusritschel.de>
2024-08-13 16:10:54 +0200

    Add script for data coverage AUX figure

---
+++ scripts/figure_aux_data_gaps.py
@@ -0,0 +1,70 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-08-13
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+from src import DATA_DIR
+import xarray as xr
+import pandas as pd
+import cartopy.crs as ccrs
+import calendar
+import matplotlib.pyplot as plt
+from my_code_base.plot.maps import *
+from src import *
+
+log = logging.getLogger(__name__)
+
+plt.style.use(BASE_DIR / "assets/mpl_styles/white_paper.mplstyle")
+
+
+def main():
+    data = read_data()
+    data = prepare_data(data)
+    plot_maps(data)
+    save(plt.gcf(), PLOT_DIR/"aux_data_gaps.png", add_hash=True)  # noqa: F405
+
+
+def read_data():
+    input_file = (DATA_DIR/"raw/merged/observations/pco2/monthly/").glob(
+        "pco2_MPIM-SOM-FFN_*.nc"
+    )
+    data = xr.open_dataset(next(input_file))
+    data = data["pco2"].sel(time=slice("2005", "2022")).load()
+    return data
+
+
+def prepare_data(data):
+    data /= data
+    data = data.groupby("time.month").mean("time")
+    data = data.sel(lat=slice(50, 90))
+    data = -(data.fillna(0) - 1)
+    data = data.where(data==1)
+    return data
+
+
+def plot_maps(data):
+    g = data.plot.contourf(
+        x="lon",
+        y="lat",
+        col="month",
+        col_wrap=3,
+        add_colorbar=False,
+        levels=[0, 1],
+        colors="crimson",
+        transform=ccrs.PlateCarree(),
+        subplot_kws=dict(projection=ccrs.NorthPolarStereo()),
+    )
+    for i, ax in enumerate(g.axs.flat):
+        ax.polar.add_features(
+            labels=False,
+            ocean_kwargs=dict(facecolor="#fff"),
+            land_kwargs=dict(facecolor="#ddd"),
+        )
+        ax.set_title(calendar.month_name[i + 1])
+
+
+if __name__ == "__main__":
+    main()

0f2c58f9a6df1f3a39654000dd282de192f4dd0f
markusritschel <git@markusritschel.de>
2024-08-15 18:06:04 +0200

    Add function to apply common color norm to plots

--- src/helper/graphics.py
+++ src/helper/graphics.py
@@ -5,6 +5,7 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
 import logging
+from matplotlib import pyplot as plt
 import matplotlib.colors as mcolors
 import numpy as np
 
@@ -21,4 +22,18 @@ class MidpointNorm(mcolors.Normalize):
         v_ext = np.max( [ np.abs(self.vmin), np.abs(self.vmax) ] )
         x, y = [-v_ext, self.midpoint, v_ext], [0, 0.5, 1]
         return np.ma.masked_array(np.interp(value, x, y))
+
+
+def get_common_norm(images, **kwargs):
+    """
+    Find the min and max of all colors for use in setting the color scale.
+    """
+    midpoint = kwargs.pop('midpoint', None)
+    ims = np.array([image.get_array().data for image in images])
+    vmin = np.nanquantile((ims), .02)
+    vmax = np.nanquantile((ims), .98)
+    norm = plt.Normalize(vmin=vmin, vmax=vmax)
+    if (midpoint is not None) or (np.sign(vmin) != np.sign(vmax)):
+        norm = MidpointNorm(vmin=vmin, vmax=vmax, center=midpoint or 0)
+    return norm
 

2fc585942b55f0a330bedbd88a45e85fb90c3bf4
markusritschel <git@markusritschel.de>
2024-08-16 15:41:41 +0200

    Update figure_4

--- scripts/figure_4.py
+++ scripts/figure_4.py
@@ -4,7 +4,8 @@
 # Date:   2024-08-02
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
-from calendar import calendar
+import calendar
+from cmath import rect
 import logging
 import textwrap
 import cmocean
@@ -31,77 +32,13 @@ def main():
     annual_max_month = get_month_of_annual_max(data)
     seasonal_range = get_seasonal_range(data)
 
-    # TODO: work with subfigures.?
-    # fig = plt.figure(figsize=(12,10))
-    # month_fig, ampl_fig = fig.subfigures(1, 2, width_ratios=[2,1])
-    # plot_min_max_month_maps(month_fig)
-    # plot_amplitude_maps(ampl_fig)
-    for data, name in zip([annual_min_month, annual_max_month, seasonal_range], ['annual_min_month', 'annual_max_month', 'seasonal_range']):
-        # first_5, last_5, raw_delta = build_pentads_and_delta(data)
-        data_dict = build_pentads_and_delta(data)
-
-        fig, axes = plt.subplots(1,3, subplot_kw=dict(projection=ccrs.NorthPolarStereo()))
-        
-        images = []
-        titles = iter(['2005–2009', '2017-2021', 'Delta'])
-        # for j, (da, ax) in enumerate(zip([first_5, last_5, delta], axes)):
-        for data_var, ax in zip(['first', 'last', 'delta'], axes):
-            da = data_dict[data_var]
-            long_line = f"{da[name].attrs['long_name']}"
-            if ('units' in da[name].attrs):
-                # if (data_var == 'delta'):
-                #     long_line += f" trend [{da[name].attrs['units']}/year]"
-                # else:
-                long_line += f" [{da[name].attrs['units']}]"
-            kwargs = dict(robust=False,
-                          cbar_kwargs={'label':textwrap.fill(long_line, 20), 
-                                       'shrink':.6, 
-                                       'orientation': 'horizontal'}
-                )
-            if name == 'seasonal_range':
-                kwargs['levels'] = None
-                kwargs['cmap'] = cmcrameri.cm.lipari
-            else:  # months 
-                cmap_timing = cmcrameri.cm.brocO
-                kwargs['cmap'] = cmap_timing
-                kwargs['levels'] = np.arange(1, 13)
-                # norm = mpl.colors.BoundaryNorm(np.arange(0, 13) + .5, cmap_timing.N)
-                # cax1 = fig.add_axes([0.92, 0.49, 0.02, 0.3])
-                # cbar1 = mpl.colorbar.ColorbarBase(cax1, cmap=cmap_timing, norm=norm, orientation='vertical', ticks=np.arange(1, 13))
-                # # tick_locs = (np.arange(n_clusters) + 1.5)*(n_clusters - 1)/n_clusters
-                # # cbar.set_ticks(tick_locs)
-                # # # set tick labels (as before)
-                # cbar1.set_ticklabels([calendar.month_abbr[i][:1] for i in np.arange(1,13)])
-
-
-
-            if data_var == 'delta':
-                kwargs['robust'] = True
-                kwargs['cmap'] = cmocean.cm.balance
-                if name.endswith('month'):
-                    # Adjust for cyclic nature (months from 1 to 12)
-                    da = ((da + 6) % 12) - 6
-                    kwargs['robust'] = False
-                    kwargs['levels'] = None
-            # else:
-                # kwargs['cbar_kwargs'] = None
-                # kwargs['add_colorbar'] = False
-
-            im = plot_map(da[name], ax=ax, **kwargs)
-            if data_var != 'delta':
-                images.append(im)
-            ax.set_title(next(titles))
-
-        if name == 'seasonal_range':
-            norm = get_common_norm(images)
-            for im in images:
-                im.set_norm(norm)
-
-        save(fig, PLOT_DIR/f"figure_4_{name}.png", add_hash=True, bbox_inches='tight', transparent=True)
+    plot_maps(annual_min_month, cmap=cmcrameri.cm.brocO)
+    plot_maps(annual_max_month, cmap=cmcrameri.cm.brocO)
+    plot_maps(seasonal_range, cmap=cmcrameri.cm.lipari)
 
 
 def load_data():
-    input_file = (DATA_DIR/f"raw/merged/observations/pco2/monthly/").glob(f"pco2_MPIM-SOM-FFN_*.nc")
+    input_file = (DATA_DIR/"raw/merged/observations/pco2/monthly/").glob("pco2_MPIM-SOM-FFN_*.nc")
     da = xr.open_mfdataset(input_file)['pco2'].sel(time=slice("2005", "2022")).load()
     da = da.sel(lat=slice(50, 90))
     return da
@@ -137,31 +74,90 @@ def get_seasonal_range(data):
 
 def build_pentads_and_delta(data):
     first_5 = data.isel(year=slice(0, 5)).mean('year')
+    first_5.attrs['title'] = f"{data.year.values[0]}–{data.year.values[4]}"
+
     last_5 = data.isel(year=slice(-5, None)).mean('year')
+    last_5.attrs['title'] = f"{data.year.values[-5]}–{data.year.values[-1]}"
+
     raw_delta = get_delta(first_5, last_5)
+    raw_delta.attrs['title'] = "Delta"
+
     return {'first': first_5, 
             'last': last_5, 
             'delta': raw_delta}
 
 
-def get_delta(ds1, ds2):
-    delta = ds2 - ds1
-    var = list(ds1.data_vars)[0]
-    delta[var].attrs['long_name'] = 'Δ' + delta[var].attrs['long_name']
+def get_delta(da1, da2):
+    delta = da2 - da1
+    if delta.attrs['long_name'].startswith('Month'):
+        # Adjust for cyclic nature (months from 1 to 12)
+        delta = ((delta + 6) % 12) - 6
+    delta.attrs['long_name'] = 'Δ' + delta.attrs['long_name']
     return delta
 
 
+def plot_maps(ds, cmap=None, **kwargs):
+    var = list(ds.data_vars)[0]
+    da = ds[var]
+    da_dict = build_pentads_and_delta(da)
+    
+    fig, axes = plt.subplots(1, 3, subplot_kw=dict(projection=ccrs.NorthPolarStereo()), layout='constrained')
+
+    fig.get_layout_engine().set(w_pad=.05, rect=[.05, 0, .95, 1])
+    fig.text(0, .4, textwrap.fill(da.attrs['long_name'], 15), ha='center', va='center', rotation=90, fontsize='large')
+
+    da = da_dict['first']
+    im1 = plot_map(da, ax=axes[0], cmap=cmap)
+        
+    da = da_dict['last']
+    im2 = plot_map(da,  ax=axes[1], cmap=cmap)
+        
+    da = da_dict['delta']
+    _im = plot_map(da, ax=axes[2], robust=True)
+    
+
+    if 'range' in var.lower():
+        norm = get_common_norm([im1, im2])
+        for im in [im1, im2]:
+            im.set_norm(norm)
+    # TODO: transform (maybe in a class) such that the norm is determined AND set
+
+    save(fig, PLOT_DIR/f"figure_4_{var}.png", add_hash=True, bbox_inches='tight', transparent=True)
+
+
 def plot_map(data, ax=None, **kwargs):
+    cbar_kwargs = {
+        "shrink": 0.8,
+        "orientation": "horizontal",
+        "extend": "both",
+    }
+    
+    long_name = data.long_name
+    if long_name.lower().startswith('month'):
+        cmap = kwargs.get('cmap', cmcrameri.cm.brocO)
+        norm = mpl.colors.BoundaryNorm(np.arange(0, 13) + .5, cmap.N)
+        kwargs['norm'] = norm
+
+    var = data.name
+    if var.lower().endswith('month'):
+        kwargs['robust'] = False
+        cbar_kwargs['extend'] = 'neither'
+
     ax = ax or plt.gca()
-    im = data.plot(ax=ax, transform=ccrs.PlateCarree(), **kwargs)
+    im = data.plot(
+        ax=ax, transform=ccrs.PlateCarree(), cbar_kwargs=cbar_kwargs, **kwargs
+    )
+
+    if long_name.lower().startswith('month'):
+        im.colorbar.set_ticks(np.arange(1,13), labels=[calendar.month_abbr[i][:1] for i in np.arange(1,13)], minor=False)
+        im.colorbar.minorticks_off()
+
     ax.polar.add_features(labels=False, ruler=False)
+    ax.set_title(data.attrs["title"])
     return im
 
 
 def get_common_norm(images, **kwargs):
-    """
-    Find the min and max of all colors for use in setting the color scale.
-    """
     midpoint = kwargs.pop('midpoint', None)
     ims = np.array([image.get_array().data for image in images])
     vmin = np.nanquantile((ims), .02)
